74ca51fe098a781c708e26529529a851b4f63e6f
nothing <ddwi2504@gmail.com>
2023-04-26 18:39:59 +0800

    six in a row for testing on the phone

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,52 @@
 import type { NextPage } from "next";
 import styles from "../styles/card.module.css";
+import { useRef } from "react";
 
 const Home: NextPage = () => {
+const noButtonRef = useRef<HTMLButtonElement>(null);
+const cardRef = useRef<HTMLDivElement>(null);
+
+   const handleClick = () => {
+			if (noButtonRef.current && cardRef.current) {
+				const noButton = noButtonRef.current;
+				const buttonRect = noButton.getBoundingClientRect();
+				const maxX = 100
+				const maxY = 100
+				let x = Math.floor(Math.random() * maxX);
+				let y = Math.floor(Math.random() * maxY);
+
+				// Make sure the button stays within the card
+				x = Math.max(x, 0);
+				y = Math.max(y, 0);
+
+				noButton.style.transition = "transform 0.2s ease-in-out"
+				noButton.style.transform = `translateX(${x}px) translateY(${y}px)`;
+				console.log(`New button position: (${x}, ${y})`);
+			}
+		};
+
 	return (
 		<div className={styles.container}>
 			<div className={styles.card}>
-				<div className={`${styles.indexCard} ${styles.allCards}`}>
+				<div className={`${styles.indexCard} ${styles.allCards}`} ref={cardRef}>
 					<div className={styles.cardImage} />
-					<h1>This is a card</h1>
+					<h1>Apakah kamu mau setiap hari dikasih tugas?</h1>
 					<p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
-						quaerat illo cumque, quos molestias, deserunt voluptatibus quasi
-						alias doloremque odio sunt sit modi praesentium minus hic incidunt
-						nihil debitis fugiat.
+						kalo mau silahkan di klik tombol di bawah kalo tidak mau sih boleh
+						boleh saja silahkan kalo saya sarankan lebih baik iyain aja itupun
+						kalo mau gak maksa saya :)
 					</p>
-					<button className={`${styles.cardButton} ${styles.cardYesButton}`}>
-						yes
-					</button>
-					<button className={`${styles.cardButton} ${styles.cardNoButton}`}>
-						no
-					</button>
+
+					<div className={styles.containerButton}>
+						<button className={`${styles.cardButton}`}>yes</button>
+						<button
+							className={`${styles.cardButton}`}
+							ref={noButtonRef}
+							onClick={handleClick}
+						>
+							no
+						</button>
+					</div>
 				</div>
 				<div className={`${styles.allCards}`}></div>
 				<div className={`${styles.allCards}`}></div>
